d2f58d481f24eaec6a1ee9fcab769816d2929e3d
thankgod744 <chukwudilethankgod12@gmail.com>
2023-07-14 05:10:35 -0700

    scripts for c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - A c program that print a line to the standard error
+ *
+ * Return: Always 1 (Success)
+ */
+int main(void)
+{
+fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19,\n", stdout);
+return (0);
+}
