6641be2c7b8d22c16e6fd2f949a6a4acc0e52b9b
thankgod744 <chukwudilethankgod12@gmail.com>
2023-08-02 10:23:34 -0700

    obi 1

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * Description: _print_rev_recursion - print astring in reverse
+ * @s: the string to reverse
+ *
+ * Return: nothing.
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return;
+	}
+
+	s++;
+	_print_rev_recursion(s);
+	s--;
+	_putchar(*s);
+}
